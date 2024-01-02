caf588e8d58f2b2941481790d09e4976fa512113
umdoyuun <132773421+umdoyuun@users.noreply.github.com>
2024-01-02 18:56:42 +0900

    [Gold IV] Title: 그리드 네트워크, Time: 484 ms, Memory: 12344 KB, Score: 13 point -BaekjoonHub

--- 백준/Gold/18769. 그리드 네트워크/그리드 네트워크.cc
+++ 백준/Gold/18769. 그리드 네트워크/그리드 네트워크.cc
@@ -1,76 +1,106 @@
 #include <iostream>
-#include <algorithm>
+#include <string>
+#include <cmath>
+#include <set>
+#include <map>
+#include <stack>
+#include <queue>
 #include <vector>
-using namespace std;
+#include <algorithm>
+#include <climits>
+#define FASTIO cin.tie(NULL); cout.tie(NULL); ios::sync_with_stdio(false);
+#define MAX 250005
+#define LL long long
+#define INF 1e9
 
-struct node {
-	int x, y, cost;
+using namespace std;
+struct INFO {
+	int Start, End, Cost;
 };
 
-int par[25000];
-int n, m, result;
-vector<node> v;
+int T, R, C;
+vector<INFO> Edge;
+int Parent[MAX];
+int answer;
 
-bool compare(node x, node y) {
-	return x.cost < y.cost;
+void Init() {
+	for (int i = 0; i < (R * C); i++) {
+		Parent[i] = i;
+	}
+	Edge.clear();
+	answer = 0;
 }
 
-void init() {
-	result = 0;
-	v.clear();
+int Find(int X) {
+	if (Parent[X] == X) {
+		return X;
+	}
+	return Parent[X] = Find(Parent[X]);
 }
 
-int getParent(int x) {
-	if (x == par[x]) return x;
-	else return par[x] = getParent(par[x]);
+void Union(int X, int Y) {
+	int PX = Find(X);
+	int PY = Find(Y);
+	if (PX < PY) {
+		Parent[PY] = PX;
+	}
+	else {
+		Parent[PX] = PY;
+	}
 }
 
-bool unionParent(int x, int y) {
-	x = getParent(x);
-	y = getParent(y);
-	if (x == y) return false;
-	par[x] = y;
-	return true;
+bool Comp(INFO A, INFO B) {
+	return (A.Cost < B.Cost);
+}
+
+void Settings() {
+	for (int i = 0; i < Edge.size(); i++) {
+		int S = Edge[i].Start;
+		int E = Edge[i].End;
+		int Cost = Edge[i].Cost;
+		if (Find(S) != Find(E)) {
+			Union(S, E);
+			answer += Cost;
+		}
+	}
+}
+
+void Find_Answer() {
+	cout << answer << "\n";
+}
+
+void Input() {
+	cin >> T;
+	while (T--) {
+		cin >> R >> C;
+		Init();
+		for (int i = 0; i < R; i++) {
+			for (int j = 0; j < C - 1; j++) {
+				int Cost;
+				cin >> Cost;
+				int S = (i * C) + j;
+				int E = (i * C) + j + 1;
+				Edge.push_back({ S,E,Cost });
+			}
+		}
+		for (int i = 0; i < (R - 1); i++) {
+			for (int j = 0; j < C; j++) {
+				int Cost;
+				cin >> Cost;
+				int S = (i * C) + j;
+				int E = ((i + 1) * C) + j;
+				Edge.push_back({ S,E,Cost });
+			}
+		}
+		sort(Edge.begin(), Edge.end(), Comp);
+		Settings();
+		Find_Answer();
+	};
 }
 
 int main() {
-	ios::sync_with_stdio(0);
-	cin.tie(0);
-	cout.tie(0);
-	int T;
-	cin >> T;
-	for (int tc = 1; tc <= T; tc++) {
-		init();
-		cin >> n >> m;
-		for (int i = 0; i < n * m; i++) {
-			par[i] = i;
-		}
-		for (int i = 0; i < n; i++) {
-			for (int j = 0; j < m - 1; j++) {
-				int cost;
-				cin >> cost;
-				v.push_back({ (i * m) + j, (i * m) + j + 1, cost });
-				/*v[(i * m) + j].push_back({ (i * m) + j + 1, cost });
-				v[(i * m) + j + 1].push_back({ (i * m) + j, cost });*/
-			}
-		}
-		for (int i = 0; i < n - 1; i++) {
-			for (int j = 0; j < m; j++) {
-				int cost;
-				cin >> cost;
-				v.push_back({ (i * m) + j, (i * m) + j + m, cost });
-				/*v[(i * m) + j].push_back({ (i * m) + j + m, cost });
-				v[(i * m) + j + m].push_back({ (i * m + j),cost });*/
-			}
-		}
-		sort(v.begin(), v.end(), compare);
-		for (int i = 0; i < v.size(); i++) {
-			if (unionParent(v[i].x, v[i].y)) {
-				result += v[i].cost;
-				//cout << v[i].x << ' ' << v[i].y << ' ' << v[i].cost << '\n';
-			}
-		}
-		cout << result << '\n';
-	}
+	FASTIO
+	Input();
+
 	return 0;
 }
