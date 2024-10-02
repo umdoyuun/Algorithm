291f5e547b1437a56ea313f4ceac86e176bde416
umdoyuun <132773421+umdoyuun@users.noreply.github.com>
2024-10-03 00:08:35 +0900

    [Gold III] Title: 크게 만들기, Time: 12 ms, Memory: 2920 KB -BaekjoonHub

---
+++ 백준/Gold/2812. 크게 만들기/크게 만들기.cc
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <string>
+#include <deque>
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+	int n, m, cnt = 0;
+	string str;
+	deque<char> dq;
+	cin >> n >> m >> str;
+	dq.push_back(str[0]);
+	for (int i = 1; i < n; i++) {
+		while (!dq.empty()) {
+			if (dq.back() < str[i] && cnt < m) {
+				dq.pop_back();
+				cnt++;
+			}
+			else break;
+		}
+		dq.push_back(str[i]);
+	}
+	for (int i = 0; i < n - m; i++) {
+		cout << dq[i];
+	}
+	return 0;
+}
