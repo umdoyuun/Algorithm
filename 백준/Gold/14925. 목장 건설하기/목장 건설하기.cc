2e6388a76cab71162a9c9284f2c1e156e07d811d
umdoyuun <132773421+umdoyuun@users.noreply.github.com>
2024-10-31 10:20:27 +0900

    [Gold IV] Title: 목장 건설하기, Time: 52 ms, Memory: 5812 KB -BaekjoonHub

--- 백준/Gold/14925. 목장 건설하기/목장 건설하기.cc
+++ 백준/Gold/14925. 목장 건설하기/목장 건설하기.cc
@@ -2,19 +2,18 @@
 #include <algorithm>
 using namespace std;
 
-int n, m, res;
-int map[1001][1001];
-int dp[1001][1001];
-
 int main() {
 	ios::sync_with_stdio(0);
 	cin.tie(0);
 	cout.tie(0);
+	int n, m, res = 0;
+	int dp[1001][1001] = { 0, };
 	cin >> n >> m;
-	for (int i = 1; i <= n; i++) {
-		for (int j = 1; j <= m; j++) {
-			cin >> map[i][j];
-			if (map[i][j] == 0) {
+	for (register int i = 1; i <= n; i++) {
+		for (register int j = 1; j <= m; j++) {
+			int x;
+			cin >> x;
+			if (x == 0) {
 				dp[i][j] = min({ dp[i][j - 1], dp[i - 1][j], dp[i - 1][j - 1] }) + 1;
 				res = max(dp[i][j], res);
 			}
