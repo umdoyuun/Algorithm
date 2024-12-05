c4ad228edb79ba299cca47552f2899fa159f1305
umdoyuun <132773421+umdoyuun@users.noreply.github.com>
2024-12-05 16:01:22 +0900

    [Bronze V] Title: 문자열, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/9086. 문자열/문자열.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+    for(int i = 0; i < n; i++){
+        string str;
+        cin >> str;
+        cout << str[0] << str[str.size() - 1] << '\n';
+    }
+    return 0;
+}
