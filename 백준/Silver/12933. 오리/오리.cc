80b075b4c09bd7f77b65e4f3c483f24a553eb5a3
umdoyuun <132773421+umdoyuun@users.noreply.github.com>
2025-01-22 14:34:16 +0900

    [Silver II] Title: 오리, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

---
+++ 백준/Silver/12933. 오리/오리.cc
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <map>
+#include <algorithm>
+using namespace std;
+
+string str;
+int cnt[5], res;
+map<char, int> m;
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+	m['q'] = 0;
+	m['u'] = 1;
+	m['a'] = 2;
+	m['c'] = 3;
+	m['k'] = 4;
+	cin >> str;
+	for (char c : str) {
+		int x = m[c];
+		if (x == 0) {
+			cnt[x]++;
+		}
+		else {
+			if (cnt[x - 1] > cnt[x]) cnt[x]++;
+			else {
+				res = -1;
+				break;
+			}
+		}
+		if (x == 4 && cnt[4] > 0) {
+			int tmp = cnt[4];
+			for (int i = 0; i < 5; i++) {
+				res = max(cnt[i], res);
+				cnt[i] -= tmp;
+			}
+		}
+	}
+	for (int i = 0; i < 5;i++) {
+		if (cnt[i]) res = -1;
+	}
+	cout << res;
+	return 0;
+}
