03264fe85d6f1837939a4d9db925060eb0fdc282
umdoyuun <132773421+umdoyuun@users.noreply.github.com>
2025-03-04 17:33:49 +0900

    [Gold V] Title: 강의실, Time: 52 ms, Memory: 4860 KB -BaekjoonHub

---
+++ 백준/Gold/1374. 강의실/강의실.cc
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <queue>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int n;
+struct lecture {
+	int x, s, e;
+};
+
+bool cmp(lecture a, lecture b) {
+	if (a.s == b.s) {
+		return a.e < b.e;
+	}
+	return a.s < b.s;
+}
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+	cin >> n;
+	vector<lecture> v(n);
+	for (int i = 0; i < n; i++) {
+		cin >> v[i].x >> v[i].s >> v[i].e;
+	}
+	sort(v.begin(), v.end(), cmp);
+	int res = 0;
+	priority_queue<pair<int, int>> pq;
+	for (int i = 0; i < n; i++) {
+		pq.push({ -v[i].e, -v[i].s });
+		while (-pq.top().first <= v[i].s) {
+			pq.pop();
+		}
+		res = max((int)pq.size(), res);
+	}
+	cout << res << '\n';
+	return 0;
+}
