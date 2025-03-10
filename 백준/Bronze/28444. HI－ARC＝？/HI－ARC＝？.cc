b7f286df81a3f472d90736b4e2c065d3df7c6f60
umdoyuun <132773421+umdoyuun@users.noreply.github.com>
2025-03-10 17:59:13 +0900

    [Bronze V] Title: HI-ARC=?, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/28444. HI－ARC＝？/HI－ARC＝？.cc
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  int h, i, a, r, c;
+  cin >> h >> i >> a >> r >> c;
+  cout << h * i - a * r * c;
+  return 0;
+}
