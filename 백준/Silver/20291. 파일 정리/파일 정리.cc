4e16f32c50ce0394c94f9c9fdf65271bc2a0d1a4
umdoyuun <132773421+umdoyuun@users.noreply.github.com>
2025-03-16 20:40:36 +0900

    [Silver III] Title: 파일 정리, Time: 40 ms, Memory: 17360 KB -BaekjoonHub

--- 백준/Silver/20291. 파일 정리/파일 정리.cc
+++ 백준/Silver/20291. 파일 정리/파일 정리.cc
@@ -1,133 +1,141 @@
-#include <bits/stdc++.h>
-#define fastio ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
-using namespace std;
+#include <cstdio>
+#include <cstring>
+#include <cstdlib>
 
-/////////////////////////////////////////////////////////////////////////////////////////////
-/*
- * Author : jinhan814
- * Date : 2021-04-16
- * Source : https://blog.naver.com/jinhan814/222266396476
- * Description : FastIO implementation for cin, cout.
- */
-constexpr int SZ = 1 << 20;
+// 단순 해시맵 구현 (문자열->정수)
+struct HashEntry {
+    char ext[101];
+    int count;
+    int next;  // 충돌 해결을 위한 링크드 리스트
+};
 
-class INPUT {
-private:
-	char read_buf[SZ];
-	int read_idx, next_idx;
-	bool __END_FLAG__, __GETLINE_FLAG__;
-public:
-	explicit operator bool() { return !__END_FLAG__; }
-	bool is_blank(char c) { return c == ' ' || c == '\n'; }
-	bool is_end(char c) { return c == '\0'; }
-	char _readChar() {
-		if (read_idx == next_idx) {
-			next_idx = fread(read_buf, sizeof(char), SZ, stdin);
-			if (next_idx == 0) return 0;
-			read_idx = 0;
-		}
-		return read_buf[read_idx++];
-	}
-	char readChar() {
-		char ret = _readChar();
-		while (is_blank(ret)) ret = _readChar();
-		return ret;
-	}
-	template<typename T> T readInt() {
-		T ret = 0; char cur = _readChar(); bool flag = 0;
-		while (is_blank(cur)) cur = _readChar();
-		if (cur == '-') flag = 1, cur = _readChar();
-		while (!is_blank(cur) && !is_end(cur)) ret = 10 * ret + cur - '0', cur = _readChar();
-		if (is_end(cur)) __END_FLAG__ = 1;
-		return flag ? -ret : ret;
-	}
-	string readString() {
-		string ret; char cur = _readChar();
-		while (is_blank(cur)) cur = _readChar();
-		while (!is_blank(cur) && !is_end(cur)) ret.push_back(cur), cur = _readChar();
-		if (is_end(cur)) __END_FLAG__ = 1;
-		return ret;
-	}
-	double readDouble() {
-		string ret = readString();
-		return stod(ret);
-	}
-	string getline() {
-		string ret; char cur = _readChar();
-		while (cur != '\n' && !is_end(cur)) ret.push_back(cur), cur = _readChar();
-		if (__GETLINE_FLAG__) __END_FLAG__ = 1;
-		if (is_end(cur)) __GETLINE_FLAG__ = 1;
-		return ret;
-	}
-	friend INPUT& getline(INPUT& in, string& s) { s = in.getline(); return in; }
-} _in;
+// 최대 50,000개의 파일, 각 확장자 최대 100자
+HashEntry entries[50001];
+int hashTable[100003];  // 소수로 선택하여 충돌 감소
+int entryCount = 0;
 
-class OUTPUT {
-private:
-	char write_buf[SZ];
-	int write_idx;
-public:
-	~OUTPUT() { bflush(); }
-	void bflush() {
-		fwrite(write_buf, sizeof(char), write_idx, stdout);
-		write_idx = 0;
-	}
-	void writeChar(char c) {
-		if (write_idx == SZ) bflush();
-		write_buf[write_idx++] = c;
-	}
-	template<typename T> int getSize(T n) {
-		int ret = 1;
-		if (n < 0) n = -n;
-		while (n >= 10) ret++, n /= 10;
-		return ret;
-	}
-	template<typename T> void writeInt(T n) {
-		int sz = getSize(n);
-		if (write_idx + sz >= SZ) bflush();
-		if (n < 0) write_buf[write_idx++] = '-', n = -n;
-		for (int i = sz - 1; i >= 0; i--) write_buf[write_idx + i] = n % 10 + '0', n /= 10;
-		write_idx += sz;
-	}
-	void writeString(string s) { for (auto& c : s) writeChar(c); }
-	void writeDouble(double d) { writeString(to_string(d)); }
-} _out;
+// 간단한 해시 함수
+int hashString(const char* str) {
+    unsigned long hash = 5381;
+    int c;
+    while ((c = *str++))
+        hash = ((hash << 5) + hash) + c;  // hash * 33 + c
+    return hash % 100003;
+}
 
-/* operators */
-INPUT& operator>> (INPUT& in, char& i) { i = in.readChar(); return in; }
-INPUT& operator>> (INPUT& in, string& i) { i = in.readString(); return in; }
-template<typename T, typename std::enable_if_t<is_integral_v<T>>* = nullptr>
-INPUT& operator>> (INPUT& in, T& i) { i = in.readInt<T>(); return in; }
-template<typename T, typename std::enable_if_t<is_floating_point_v<T>>* = nullptr>
-INPUT& operator>> (INPUT& in, T& i) { i = in.readDouble(); return in; }
-OUTPUT& operator<< (OUTPUT& out, char i) { out.writeChar(i); return out; }
-OUTPUT& operator<< (OUTPUT& out, string i) { out.writeString(i); return out; }
-template<typename T, typename std::enable_if_t<is_integral_v<T>>* = nullptr>
-OUTPUT& operator<< (OUTPUT& out, T i) { out.writeInt<T>(i); return out; }
-template<typename T, typename std::enable_if_t<is_floating_point_v<T>>* = nullptr>
-OUTPUT& operator<< (OUTPUT& out, T i) { out.writeDouble(i); return out; }
-
-/* macros */
-#define fastio 1
-#define cin _in
-#define cout _out
-#define istream INPUT
-#define ostream OUTPUT
-/////////////////////////////////////////////////////////////////////////////////////////////
-int main(){
-    fastio;
-    int n;
-    string str;
-    map<string, int> m;
-    cin >> n;
-    for(int i = 0; i < n; i++){
-        cin >> str;
-        int j = str.find('.');
-        str = str.substr(j + 1);
-        m[str]++;
+// 해시맵에 확장자 추가 또는 카운트 증가
+void addExtension(char* ext) {
+    int hash = hashString(ext);
+    
+    // 해시 테이블에서 체인 검색
+    int idx = hashTable[hash];
+    while (idx != -1) {
+        if (strcmp(entries[idx].ext, ext) == 0) {
+            entries[idx].count++;
+            return;
+        }
+        idx = entries[idx].next;
     }
-    for(auto iter = m.begin(); iter != m.end(); iter++){
-        cout << iter->first << ' ' << iter->second << '\n';
+    
+    // 새 항목 추가
+    strcpy(entries[entryCount].ext, ext);
+    entries[entryCount].count = 1;
+    entries[entryCount].next = hashTable[hash];
+    hashTable[hash] = entryCount++;
+}
+
+// 두 확장자 비교 함수 (qsort용)
+int compareEntries(const void* a, const void* b) {
+    return strcmp(((HashEntry*)a)->ext, ((HashEntry*)b)->ext);
+}
+
+int main() {
+    // 초기화
+    memset(hashTable, -1, sizeof(hashTable));
+    
+    // 입출력 버퍼
+    char buffer[5000100];  // 최대 50,000개 파일, 각 100자 + 여유공간
+    char outputBuffer[5000100];
+    char *pR = buffer, *pW = outputBuffer;
+    
+    // 입력 읽기
+    int readSize = fread(buffer, 1, 5000000, stdin);
+    buffer[readSize] = 0;
+    
+    // N 읽기
+    int N = 0;
+    while (*pR >= '0' && *pR <= '9') {
+        N = N * 10 + (*pR - '0');
+        pR++;
     }
+    pR++; // 개행 건너뛰기
+    
+    // 파일명 처리
+    for (int i = 0; i < N; i++) {
+        char filename[101];
+        int fileLen = 0;
+        
+        // 파일명 읽기 (최대 100자)
+        while (*pR && *pR != '\n' && fileLen < 100) {
+            filename[fileLen++] = *pR++;
+        }
+        filename[fileLen] = '\0';
+        
+        // 줄의 끝까지 이동 (100자 초과한 경우 처리)
+        while (*pR && *pR != '\n') pR++;
+        if (*pR == '\n') pR++;
+        
+        // 마지막 '.' 위치 찾기
+        int dotPos = -1;
+        for (int j = 0; j < fileLen; j++) {
+            if (filename[j] == '.') dotPos = j;
+        }
+        
+        // 확장자가 있으면 해시맵에 추가
+        if (dotPos != -1) {
+            addExtension(filename + dotPos + 1);
+        }
+    }
+    
+    // 결과 정렬 (사전순)
+    qsort(entries, entryCount, sizeof(HashEntry), compareEntries);
+    
+    // 결과 출력
+    for (int i = 0; i < entryCount; i++) {
+        // 확장자 쓰기
+        char* ext = entries[i].ext;
+        while (*ext) *pW++ = *ext++;
+        
+        // 공백 추가
+        *pW++ = ' ';
+        
+        // 카운트 변환 및 쓰기
+        int count = entries[i].count;
+        
+        // 숫자를 문자열로 변환 (역순)
+        char countStr[10];
+        int countLen = 0;
+        
+        if (count == 0) {
+            countStr[countLen++] = '0';
+        } else {
+            while (count > 0) {
+                countStr[countLen++] = (count % 10) + '0';
+                count /= 10;
+            }
+        }
+        
+        // 역순으로 복사
+        for (int j = countLen - 1; j >= 0; j--) {
+            *pW++ = countStr[j];
+        }
+        
+        // 개행 추가
+        *pW++ = '\n';
+    }
+    
+    // 결과 출력
+    fwrite(outputBuffer, 1, pW - outputBuffer, stdout);
+    
     return 0;
 }
