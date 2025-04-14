ee1f88bda9a6159cb64db94400091fd06dbf75d6
umdoyuun <132773421+umdoyuun@users.noreply.github.com>
2025-04-14 12:03:49 +0900

    [Bronze V] Title: 팬들에게 둘러싸인 홍준, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/14581. 팬들에게 둘러싸인 홍준/팬들에게 둘러싸인 홍준.cc
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    string str;
+    cin >> str;
+    cout << ":fan::fan::fan:\n";
+    cout << ":fan::" << str << "::fan:\n";
+    cout << ":fan::fan::fan:\n";
+}
