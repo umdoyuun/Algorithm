4e53000857ca1362df658e146e8888d20884929d
umdoyuun <132773421+umdoyuun@users.noreply.github.com>
2025-05-12 03:21:42 +0900

    [Bronze V] Title: 자동완성, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/24883. 자동완성/자동완성.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    char c;
+    cin >> c;
+    if(c == 'N' || c == 'n'){
+        cout << "Naver D2\n";
+    }
+    else{
+        cout << "Naver Whale\n";
+    }
+    return 0;
+}
