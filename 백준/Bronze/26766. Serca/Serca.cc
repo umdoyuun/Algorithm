61a33d93b8a4b8257773a4be2ed867e9223d4c56
umdoyuun <132773421+umdoyuun@users.noreply.github.com>
2025-05-24 20:34:48 +0900

    [Bronze V] Title: Serca, Time: 164 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/26766. Serca/Serca.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int t;
+    cin >> t;
+    while(t--){
+        cout << " @@@   @@@ \n";
+        cout << "@   @ @   @\n";
+        cout << "@    @    @\n";
+        cout << "@         @\n";
+        cout << " @       @ \n";
+        cout << "  @     @  \n";
+        cout << "   @   @   \n";
+        cout << "    @ @    \n";
+        cout << "     @     \n";
+    }
+}
