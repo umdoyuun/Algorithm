2eb23b89a667aefdab8dd21af23bca90bf2c1daf
umdoyuun <132773421+umdoyuun@users.noreply.github.com>
2025-08-21 04:59:13 +0900

    [Silver IV] Title: 좋은 단어, Time: 4 ms, Memory: 2588 KB -BaekjoonHub

---
+++ 백준/Silver/3986. 좋은 단어/좋은 단어.cc
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+
+char stack[100001];
+int t, res; //  t: top
+
+void init() {
+	t = -1;
+}
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+	int n;
+	cin >> n;
+	while (n--) {
+		init();
+		string str;
+		cin >> str;
+		stack[++t] = str[0];
+		for (int i = 1; i < str.size(); i++) {
+			if (t >= 0 && str[i] == stack[t]) {
+				t--;
+				while (t >= 1) {
+					if (stack[t] == stack[t - 1]) {
+						t = t - 2;
+					}
+					else {
+						break;
+					}
+				}
+			}
+			else {
+				stack[++t] = str[i];
+			}
+		}
+		if (t == -1) res++;
+	}
+	cout << res << '\n';
+	return 0;
+}
