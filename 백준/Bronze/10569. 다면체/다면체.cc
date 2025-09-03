6948b80f60abcdd32c43e9ae32b3cd98c6797404
umdoyuun <132773421+umdoyuun@users.noreply.github.com>
2025-09-04 01:15:21 +0900

    [Bronze IV] Title: 다면체, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10569. 다면체/다면체.cc
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n, a, b;
+    cin >> n;
+    for(int i = 0; i < n; i++){
+        cin >> a >> b;
+        cout << b - a + 2 << '\n';
+    }
+}
