75011a913d632a0e3bb90b2516c26921c2b3c0fe
umdoyuun <132773421+umdoyuun@users.noreply.github.com>
2025-09-07 02:04:21 +0900

    [Bronze IV] Title: 줄번호, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/4470. 줄번호/줄번호.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+    int n;
+    string str;
+    cin >> n;
+    cin.ignore();
+    for(int i = 1;i <= n; i++){
+        getline(cin, str);
+        cout << i << ". " << str << '\n';
+    }
+}
