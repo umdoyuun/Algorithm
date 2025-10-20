922d9857e4bd4cf2b5436552530c21e19ece67be
umdoyuun <132773421+umdoyuun@users.noreply.github.com>
2025-10-21 00:34:27 +0900

    [Bronze IV] Title: 히스토그램, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/13752. 히스토그램/히스토그램.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+    for(int i = 0; i < n; i++){
+        int x;
+        cin >> x;
+        for(int j = 0; j < x; j++){
+            cout << "=";
+        }
+        cout << '\n';
+    }
+}
