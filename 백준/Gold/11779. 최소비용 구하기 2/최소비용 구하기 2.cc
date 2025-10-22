a5c3c5f7fad51e47a37a247e95cd5bbe1fd57870
umdoyuun <132773421+umdoyuun@users.noreply.github.com>
2025-10-22 10:40:40 +0900

    [Gold III] Title: 최소비용 구하기 2, Time: 24 ms, Memory: 4752 KB -BaekjoonHub

--- 백준/Gold/11779. 최소비용 구하기 2/최소비용 구하기 2.cc
+++ 백준/Gold/11779. 최소비용 구하기 2/최소비용 구하기 2.cc
@@ -1,18 +1,18 @@
 #include <iostream>
-#include <queue>
 #include <vector>
+#include <queue>
 using namespace std;
-#define INF 1e9 + 7
+#define MAX 1e9
 
-int n, m, s, e;
-vector<pair<int, int>> v[1001];
-int path[1001];
+int n, m, s, e, cnt;
+vector<pair<int, int>> v[1000];
 int dist[1001];
+int path[1001];
 
-
+int dat[1000];
 void dijkstra() {
 	for (int i = 1; i <= n; i++) {
-		dist[i] = INF;
+		dist[i] = MAX;
 	}
 	dist[s] = 0;
 	priority_queue<pair<int, int>> pq;
@@ -22,28 +22,23 @@ void dijkstra() {
 		int cost = -pq.top().first;
 		pq.pop();
 		if (x == e) break;
-		if (cost > dist[x]) continue;
-		for (int i = 0; i < v[x].size(); i++) {
-			int y = v[x][i].first;
-			int n_cost = cost + v[x][i].second;
-			if (n_cost >= dist[y]) continue;
-			else {
-				path[y] = x;
-				dist[y] = n_cost;
-				pq.push({ -n_cost, y });
-			}			
+		for (pair<int, int> next : v[x]) {
+			int y = next.first;
+			int ncost = next.second + cost;
+			if (dist[y] <= ncost) continue;
+			dist[y] = ncost;
+			path[y] = x;
+			pq.push({ -ncost, y });
 		}
 	}
-	int idx = e;
-	vector<int> res;
-	while (s != idx) {
-		res.push_back(idx);
-		idx = path[idx];
-	}
-	res.push_back(s);
-	cout << dist[e] << '\n' << res.size() << '\n';
-	for (int i = res.size() - 1; i >= 0; i--) {
-		cout << res[i] << ' ';
+}
+
+void path_trace() {
+	int cur = e;
+	dat[cnt++] = cur;
+	while (cur != s) {
+		cur = path[cur];
+		dat[cnt++] = cur;
 	}
 }
 
@@ -55,9 +50,14 @@ int main() {
 	for (int i = 0; i < m; i++) {
 		int x, y, cost;
 		cin >> x >> y >> cost;
-		v[x].push_back({ y ,cost });
+		v[x].push_back({y, cost});
 	}
 	cin >> s >> e;
 	dijkstra();
+	path_trace();
+	cout << dist[e] << '\n' << cnt << '\n';
+	for (int i = cnt - 1; i >= 0; i--) {
+		cout << dat[i] << ' ';
+	}
 	return 0;
 }
