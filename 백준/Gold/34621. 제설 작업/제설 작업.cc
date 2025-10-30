880b0fa99a3e415d1475ad962874492c557e3224
umdoyuun <132773421+umdoyuun@users.noreply.github.com>
2025-10-30 14:11:35 +0900

    [Gold IV] Title: 제설 작업, Time: 464 ms, Memory: 17676 KB -BaekjoonHub

---
+++ 백준/Gold/34621. 제설 작업/제설 작업.cc
@@ -0,0 +1,67 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+const int mx = 0x7fffffff;
+int n, m, res, sz;
+int graph[2001][2001];
+int sum[2001][2];
+long long total;
+
+void snow_cleaning() {
+	int flag = 0;
+	int mn = mx;
+	int idx = 0;
+	for (int i = 0; i < sz; i++) {
+		if (!sum[i][0]) continue;
+		if (mn > sum[i][0]) {
+			mn = sum[i][0];
+			idx = i;
+		}
+	}
+	for (int i = 0; i < sz; i++) {
+		if (!sum[i][1]) continue;
+		if (mn > sum[i][1]) {
+			flag = 1;
+			mn = sum[i][1];
+			idx = i;
+		}
+	}
+	total -= mn;
+	res = max(res, mn);
+	
+	if (flag) {
+		for (int i = 0; i < sz; i++) {
+			sum[i][0] -= graph[idx][i];
+			graph[idx][i] = 0;
+		}
+	}
+	else {
+		for (int i = 0; i < sz; i++) {
+			sum[i][1] -= graph[i][idx];
+			graph[i][idx] = 0;
+		}
+	}
+	sum[idx][flag] = 0;
+}
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+	cin >> n >> m;
+	sz = max(n, m);
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			cin >> graph[i][j];
+			total += graph[i][j];
+			sum[j][0] += graph[i][j];
+			sum[i][1] += graph[i][j];
+		}
+	}
+	while (total) {
+		snow_cleaning();
+	}
+	cout << res << '\n';
+	return 0;
+}
