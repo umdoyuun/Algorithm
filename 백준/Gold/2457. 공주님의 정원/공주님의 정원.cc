972bfe34f7d866d3b2416ec8ce2b37c86135aad9
umdoyuun <132773421+umdoyuun@users.noreply.github.com>
2025-10-30 12:12:47 +0900

    [Gold III] Title: 공주님의 정원, Time: 32 ms, Memory: 2800 KB -BaekjoonHub

--- 백준/Gold/2457. 공주님의 정원/공주님의 정원.cc
+++ 백준/Gold/2457. 공주님의 정원/공주님의 정원.cc
@@ -3,61 +3,51 @@
 using namespace std;
 
 struct flower {
-	int sm, sd, em, ed;
+    int start, end;
 };
 flower f[100001];
 
-bool cmp(flower a, flower b) {
-	if (a.sm == b.sm) {
-		if (a.sd == b.sd) {
-			if (a.em == b.em) {
-				return a.ed > b.ed;
-			}
-			return a.em > b.em;
-		}
-		return a.sd < b.sd;
-	}
-	return a.sm < b.sm;
+bool cmp(const flower& a, const flower& b) {
+    if (a.start == b.start) return a.end > b.end;
+    return a.start < b.start;
 }
 
 int main() {
-	ios::sync_with_stdio(0);
-	cin.tie(0);
-	cout.tie(0);
-	int m = 3, d = 1, n, res = 0, x = 0, y = 0;
-	cin >> n;
-	int sz = 0;
-	for (int i = 0; i < n; i++) {
-		int sm, sd, em, ed;
-		cin >> sm >> sd >> em >> ed;
-		if (em < 3 || sm > 11) continue;
-		f[sz++] = { sm, sd, em, ed };
-	}
-	sort(f, f + sz, cmp);
-	
-	for (int i = 0; i < sz; i++) {
-		if (m > 12) {
-			break;
-		}
-		if (m < f[i].sm || (m == f[i].sm && d < f[i].sd)) {
-			m = x;
-			d = y;
-			res++;
-		}
-		if (m > f[i].sm || (m == f[i].sm && d >= f[i].sd)) {
-			if (x < f[i].em || (x == f[i].em && y <= f[i].ed)) {
-				x = f[i].em;
-				y = f[i].ed;
-			}
-		}
-		
-	}
-	if (m < 12 && m < x) {
-		res++;
-		m = x;
-		d = y;
-	}
-	if (m < 12) res = 0;
-	cout << res << '\n';
-	return 0;
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    int cur = 301, n, res = 0, maxEnd = 0;
+    cin >> n;
+    
+    int sz = 0;
+    for (int i = 0; i < n; i++) {
+        int sm, sd, em, ed;
+        cin >> sm >> sd >> em >> ed;
+        int s = sm*100 + sd, e = em*100 + ed;
+        if (e < 301 || s > 1130) continue;
+        f[sz++] = { s, e };
+    }
+    
+    sort(f, f + sz, cmp);
+    
+    int i = 0;
+    while (cur < 1201) {
+        maxEnd = 0;
+        
+        while (i < sz && f[i].start <= cur) {
+            maxEnd = max(maxEnd, f[i].end);
+            i++;
+        }
+        
+        if (maxEnd == 0) {
+            res = 0;
+            break;
+        }
+        
+        res++;
+        cur = maxEnd;
+    }
+    
+    cout << res << '\n';
+    return 0;
 }
