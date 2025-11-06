61a388d5cb94eff159deb6b59609d2c6d4370e5f
umdoyuun <132773421+umdoyuun@users.noreply.github.com>
2025-11-06 10:36:57 +0900

    [Bronze I] Title: 수 정렬하기 3, Time: 1460 ms, Memory: 2060 KB -BaekjoonHub

--- 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.cc
+++ 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.cc
@@ -1,22 +1,23 @@
 #include <iostream>
 using namespace std;
 
-int cnt[10001];
+int dat[10001];
 
-int main(){
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-    cout.tie(0);
-    register int n, x;
-    cin >> n;
-    while(n--){
-        cin >> x;
-        cnt[x]++;
-    }
-    for(int i = 1; i <= 10000; i++){
-        for(int j = 0; j < cnt[i]; j++){
-            cout << i << '\n';
-        }
-    }
-    return 0;
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+	int n;
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		int x;
+		cin >> x;
+		dat[x]++;
+	}
+	for (int i = 1; i <= 10000; i++) {
+		for (int j = 0; j < dat[i]; j++) {
+			cout << i << '\n';
+		}
+	}
+	return 0;
 }
